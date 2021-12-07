bed720c49c1c73c484a7e9f3c2b69e83cae28916
AnonymousChief04 <45501449+AnonymousChief04@users.noreply.github.com>
2021-12-07 13:31:13 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import pygame
+import os
+pygame.init()
+
+
+WIDTH, HEIGHT = 1000, 700
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Starcraft")
+
+FPS = 60
+
+def main():
+    clock = pygame.time()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        
