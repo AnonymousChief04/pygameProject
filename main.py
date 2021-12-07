50ea0a694a7f2f15b60517d377edd8eb9e0c72db
AnonymousChief04 <45501449+AnonymousChief04@users.noreply.github.com>
2021-12-07 13:35:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-https://github.com/AnonymousChief04/pygameProject.git
+import pygame
+import os
+pygame.init()
+
+
+WIDTH, HEIGHT = 1000, 700
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Starcraft")
+
+FPS = 60
+
+def main():
+    clock = pygame.time()
+
+    run = True
+    while run:
+        clock.tick(FPS)
+        
